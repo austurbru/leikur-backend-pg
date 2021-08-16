04c2d51b44baf8baa35138bc8fc9131bd5726a21
Paula Nerenberg <paulita.glam@gmail.com>
2021-08-16 18:20:16 +0000

    All sorts of changes

--- config/database.js
+++ config/database.js
@@ -1,19 +1,3 @@
-// module.exports = ({ env }) => ({
-//   defaultConnection: 'default',
-//   connections: {
-//     default: {
-//       connector: 'bookshelf',
-//       settings: {
-//         client: 'sqlite',
-//         filename: env('DATABASE_FILENAME', '.tmp/data.db'),
-//       },
-//       options: {
-//         useNullAsDefault: true,
-//       },
-//     },
-//   },
-// });
-
 module.exports = ({ env }) => ({
   defaultConnection: 'default',
   connections: {
@@ -27,7 +11,9 @@ module.exports = ({ env }) => ({
         username: env('DATABASE_USERNAME', 'strapi'),
         password: env('DATABASE_PASSWORD', 'strapi'),
       },
-      options: {},
+      options: {
+        autoMigration: true, // this option is required in dbs other than sqlite so that tables can be created autuomatically
+      },
     },
   },
 });
--- dbBackup/info.md
+++ dbBackup/info.md
@@ -1,18 +1,14 @@
-#Backups and Restore
+# Backups and Restore
 
-###Taking backup from local database. Remember to change the file name to current date.
+### Taking backup from local database. Remember to change the file name to current date.
 
 pg_dump -h localhost -p 5432 -U postgres --format=c leikur > D:\strapi-projects\leikur-strapi\dbBackup\local\2021-07-16.dump
 
-###Restoring local backup to Heroku:
-
-1. Upload the backup to Cloudinary
-2. Copy the Cloudinary URL for the backup.
-3. Do the following command in the Heroku CLI:
+### Restoring local backup to Heroku:
 
 heroku pg:backups:restore '<ClodinaryURL for the backup>' DATABASE_URL -a leikur-strapi
 
-###Getting backup from Heroku:
+### Getting backup from Heroku:
 
 1. Navigate in the terminal to:
    D:\strapi-projects\leikur-strapi\dbBackup\heroku
